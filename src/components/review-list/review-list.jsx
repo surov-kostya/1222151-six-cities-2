e1513ceceb7e0d99d149de076cf83872e9c0c02b
Surov Kostya <surov.qostya@yandex.ru>
2019-11-10 19:30:52 +0300

    Написал снэпшот-тесты для review и review-list

--- src/components/review-list/review-list.jsx
+++ src/components/review-list/review-list.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Review from '../review/review';
 import {arrayOf} from 'prop-types';
 import {reviewType} from '../../mocks/offers';
+
 const ReviewList = ({reviews}) => {
   return (
     <ul className="reviews__list">
