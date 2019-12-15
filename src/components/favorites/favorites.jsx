816567025c80c99a8b1185870352a9abeac210af
Surov Kostya <surov.qostya@yandex.ru>
2019-12-16 00:31:50 +0300

    Добавил представления для пустого списка

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -22,40 +22,55 @@ export class Favorites extends PureComponent {
       <div className="page">
         <Header />
 
-        <main className="page__main page__main--favorites">
-          <div className="page__favorites-container container">
-            <section className="favorites">
-              <h1 className="favorites__title">Saved listing</h1>
-              <ul className="favorites__list">
-                {
-                  this.favCities.map((city) => (
-                    <li key={city} className="favorites__locations-items">
-                      <div className="favorites__locations locations locations--current">
-                        <div className="locations__item">
-                          <a className="locations__item-link" href="#">
-                            <span>{city}</span>
-                          </a>
+        {this.favPlaces.length
+          ? (<main className="page__main page__main--favorites">
+            <div className="page__favorites-container container">
+              <section className="favorites">
+                <h1 className="favorites__title">Saved listing</h1>
+                <ul className="favorites__list">
+                  {
+                    this.favCities.map((city) => (
+                      <li key={city} className="favorites__locations-items">
+                        <div className="favorites__locations locations locations--current">
+                          <div className="locations__item">
+                            <a className="locations__item-link" href="#">
+                              <span>{city}</span>
+                            </a>
+                          </div>
                         </div>
-                      </div>
-                      <div className="favorites__places">
-                        {
-                          this.favPlaces.filter((place) => place.city === city).map((place) => (
-                            <PlaceCard
-                              key={place.id}
-                              place={place}
-                              onTitleClick={() => {}}
-                              onCardActivate={() => {}}
-                            />
-                          ))
-                        }
-                      </div>
-                    </li>
-                  ))
-                }
-              </ul>
-            </section>
-          </div>
-        </main>
+                        <div className="favorites__places">
+                          {
+                            this.favPlaces.filter((place) => place.city === city).map((place) => (
+                              <PlaceCard
+                                key={place.id}
+                                place={place}
+                                onTitleClick={() => {}}
+                                onCardActivate={() => {}}
+                              />
+                            ))
+                          }
+                        </div>
+                      </li>
+                    ))
+                  }
+                </ul>
+              </section>
+            </div>
+          </main>)
+
+          : (<main className="page__main page__main--favorites page__main--favorites-empty">
+            <div className="page__favorites-container container">
+              <section className="favorites favorites--empty">
+                <h1 className="visually-hidden">Favorites (empty)</h1>
+                <div className="favorites__status-wrapper">
+                  <b className="favorites__status">Nothing yet saved.</b>
+                  <p className="favorites__status-description">Save properties to narrow down search or plan yor future trips.</p>
+                </div>
+              </section>
+            </div>
+          </main>)
+        }
+
         <footer className="footer container">
           <a className="footer__logo-link" href="main.html">
             <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
@@ -64,7 +79,7 @@ export class Favorites extends PureComponent {
       </div>);
   }
 
-  conponentDidMount() {
+  componentDidMount() {
     this.fetchFavorites();
   }
 }
